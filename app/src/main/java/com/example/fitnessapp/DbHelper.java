e35c8ef7856ec51ce9e3fbc81530fb69dfa65ba2
Tyler Johnston <Hedges07@gmail.com>
2023-03-16 21:18:18 -0400

    Login Pages

---
+++ app/src/main/java/com/example/fitnessapp/DbHelper.java
@@ -0,0 +1,62 @@
+package com.example.project_login;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DbHelper extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "Login.db";
+
+    public DbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE table users(username TEXT, password TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists users");
+    }
+
+    public boolean insertData(String username, String password) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put("username", username);
+        values.put("password", password);
+
+        long result = db.insert("users", null, values);
+
+        if (result == -1) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    public boolean checkUsername (String username) {
+        SQLiteDatabase db= this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * from users WHERE username=?", new String[] {username});
+        if(cursor.getCount() > 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public boolean checkPassword (String username,String password) {
+        SQLiteDatabase db= this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM users WHERE username=? AND password=?", new String[] {username,password});
+        if(cursor.getCount() > 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/fitnessapp/loginActivity.java
@@ -0,0 +1,81 @@
+package com.example.project_login;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class loginActivity extends AppCompatActivity {
+    Button button_login, button_signup;
+    //Switch switch_remember;
+    TextView tv_username, tv_password;
+    String username, password;
+    //boolean remember;
+    DbHelper DB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        button_login = findViewById(R.id.button_Login);
+        button_signup = findViewById(R.id.button_signUp);
+        //switch_remember = findViewById(R.id.switch_remember);
+        tv_username = findViewById(R.id.text_userName);
+        tv_password = findViewById(R.id.text_password);
+        DB = new DbHelper(this);
+
+        button_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                username = tv_username.getText().toString();
+                password = tv_password.getText().toString();
+
+                if(TextUtils.isEmpty(username) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(),"Enter All Fields",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    boolean checkUser = DB.checkUsername(username);
+                    if (checkUser == false) {
+                        boolean insert = DB.insertData(username,password);
+                        if (insert) {
+                            Toast.makeText(getApplicationContext(),"Registered",Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(loginActivity.this, HomePageActivity.class);
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(),"Registration Failed",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(),"User Already Exists",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                username = tv_username.getText().toString();
+                password = tv_password.getText().toString();
+
+                if(TextUtils.isEmpty(username) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(),"Please Enter All Fields",Toast.LENGTH_SHORT).show();  ;
+                }
+                else {
+                    Boolean checkUserPass = DB.checkPassword(username,password);
+                    if(checkUserPass) {
+                        Toast.makeText(getApplicationContext(),"Logged In",Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(loginActivity.this, HomePageActivity.class);
+                        startActivity(intent);
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(),"Login Failed",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+}
