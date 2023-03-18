34f1d6d200bbd2877b89a576c909fd41c48ca0b1
marinazjalic <zjalicm@uwindsor.ca>
2023-03-18 16:49:30 -0400

    added menu and layout
    added menu nav bar, tracker layout, made recyclerview for main page

---
+++ app/src/main/java/com/example/fitnessapp/CategoryData.java
@@ -0,0 +1,16 @@
+package com.example.fitnessapp;
+
+public class CategoryData {
+
+    private String text;
+
+    public CategoryData(String text){
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+
+}
