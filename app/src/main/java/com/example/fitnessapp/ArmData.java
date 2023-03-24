daa70ca7932d6c556b6aaaf31edd0a4eea413e41
AdamTaylor47 <adam.taylor0147@gmail.com>
2023-03-23 21:48:47 -0400

    Making the workout recycler views

---
+++ app/src/main/java/com/example/fitnessapp/ArmData.java
@@ -0,0 +1,14 @@
+package com.example.fitnessapp;
+
+public class ArmData {
+
+    private String text;
+
+    public ArmData(String text){
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
